3a17f9d78031d72a263ee318c58be94babeb07cd
Bruno Costa <bruno.l.costa@gmail.com>
2021-06-04 14:32:21 -0400

    lift script

--- src/views/About.vue
+++ src/views/About.vue
@@ -3,3 +3,52 @@
     <p>Bruno Costa - My first Vue application. A site for events to better the world.</p>
   </div>
 </template>
+
+<script>
+/*
+** THIS VERSION OF LIFT.JS IS FOR ACQUIA LIFT VERSION 4
+*/
+window.AcquiaLift = {
+  /*
+  ** ACCOUNT PROPERTIES | MANDATORY | Should be the same across all sites in the same account
+  ** Content values need to be the same as what's Lift Profile Manager, Admin > Customer Sites > Customer Details
+  ** The following values are examples. Update all variables for your own installation of Acquia Lift
+  */
+  account_id: "OBIOBC",
+  liftAssetsURL: "https://builder.lift.acquia.com",
+  liftDecisionAPIURL: "https://us-east-1-demo-decisionapi.lift.acquia.com",
+  authEndpoint: "https://us-east-1-demo-oauth2.lift.acquia.com/authorize",
+  contentReplacementMode: "trusted",
+
+  /*
+  ** SITE ID | MANDATORY | Should be different for each site and environment in the same account
+  ** Content value needs to be created and match what's in Lift Profile Manager
+  ** Admin > Manage Configuration Data > Customer Sites
+  */
+  site_id: "myvueapp",
+
+  /** PAGE-SPECIFIC PROPERTIES | OPTIONAL | Content values can be different from page to page **/
+  profile: {
+    'author':"Acquia",
+    'engagement_score':'1',
+    'page_type':"content page",
+    'post_id':"123",
+    'published_date':"1507914363",
+    'content_title':"Homepage",
+    'content_type':"page",
+    'content_section':"Home",
+    'content_keywords':"LiftU",
+    'persona':"Student",
+
+    /* CUSTOM COLUMN META DATA | OPTIONAL | Content values can be different from page to page
+    ** The itemprops need to be created from Lift Profile Manager
+    ** Admin > Manage Configuration Data > Custom Column Meta Data.
+    ** Will always be the format below of {table}_udf{accessor_number} -->
+    **/
+    'person_udf4':"Custom Value"
+  }
+};
+</script>
+
+<!-- LIFT JS script | MANDATORY -->
+<script type="text/javascript" src="https://builder.lift.acquia.com/lift.js"></script>
