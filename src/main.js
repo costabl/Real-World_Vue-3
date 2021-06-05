f05acc8218466deb36e5393d5bc0c9b64ee51890
Bruno Costa <bruno.l.costa@gmail.com>
2021-06-04 22:41:49 -0400

    remove vue-meta

--- src/main.js
+++ src/main.js
@@ -2,10 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import vuemeta from 'vue-meta'
 
 createApp(App)
   .use(store)
   .use(router)
-  .use(vuemeta).config.productionTip = false
   .mount('#app')
